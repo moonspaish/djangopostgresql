2051b45bebdc37cf813220f065bfc80395fcd68e
moonspaish <69521713+moonspaish@users.noreply.github.com>
2024-07-13 11:14:24 +0300

    kinda edited build files

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,16 @@
+if ! command -v python &> /dev/null
+then
+    echo "Python could not be found"
+    exit
+fi
+
+# Check if pip is installed
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found, installing pip"
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+fi
 pip install -r requirements.txt
 python manage.py migrate 
 python manage.py collectstatic
